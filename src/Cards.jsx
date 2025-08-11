3d4c284dd09b75b762828edd31a835119e49531c
FernandoVini <>
2025-08-11 14:44:58 -0300

    estilizacao do botao de pesquisa

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -6,22 +6,22 @@ function Cards(){
         <div className="cards" >
             <div className="card1" >
                 <div className='nav' >
-                    <button>navigator</button>
+                    <h3>navigator</h3>
                 </div>
             </div>
             <div className="card2" >
                 <div className='nav' >
-                    <button>navigator</button>
+                    <h3>navigator</h3>
                 </div>
             </div>
             <div className="card3" >
                 <div className='nav' >
-                    <button>navigator</button>
+                    <h3>navigator</h3>
                 </div>
             </div>
             <div className="card4" >
                 <div className='nav' >
-                    <button>navigator</button>
+                    <h3>navigator</h3>
                 </div>
             </div>
         </div>
